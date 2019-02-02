5724810bb5c433ef8089f9c55ac8dffda0317a24
default <ykim@fa05956kyoung-tae.local>
2019-02-02 13:46:06 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by Kim, Young-Tae on 2019. 2. 2..
+//  Copyright © 2019년 David Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
